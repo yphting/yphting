b98faa5c20686225f40fc22315d18ddac8eb430d
YunoGit <1799729439@qq.com>
2018-10-24 09:55:36 +0800

    v1.0.0

---
+++ src/main/java/com/accp/util/third/ThirdInfo.java
@@ -0,0 +1,53 @@
+package com.accp.util.third;
+
+public class ThirdInfo {
+	private String name;
+
+	private String img;
+
+	private int sex;
+
+	private String uniq;
+
+	private String from;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public int getSex() {
+		return sex;
+	}
+
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+
+	public String getUniq() {
+		return uniq;
+	}
+
+	public void setUniq(String uniq) {
+		this.uniq = uniq;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+}
