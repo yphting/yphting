597dc07e8868d8350fad573daae276cedc9d1c57
YunoGit <1799729439@qq.com>
2018-10-30 14:14:58 +0800

    v1.0.0

--- src/main/java/com/accp/util/DateTime.java
+++ src/main/java/com/accp/util/DateTime.java
@@ -12,6 +12,7 @@ import java.util.Date;
 public class DateTime {
 	// 时间格式转换器
 	private static SimpleDateFormat simpleDate = new SimpleDateFormat("yyyy-MM-dd");
+	private static SimpleDateFormat simpleDateTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
 	/**
 	 * 当前时间
@@ -21,5 +22,43 @@ public class DateTime {
 	public static String nowDate() {
 		String time = simpleDate.format(new Date());
 		return time;
+	}
+
+	/**
+	 * 时间差
+	 * 
+	 * @param start
+	 *            开始日期
+	 * @param end
+	 *            结束日期
+	 * @return 天数
+	 */
+	public static long dateLag(Object start, Object end) {
+		try {
+			Date s = start instanceof Date ? (Date) start : simpleDate.parse((String) start);
+			Date e = end instanceof Date ? (Date) end : simpleDate.parse((String) end);
+			return (e.getTime() - s.getTime()) / (24 * 60 * 60 * 1000);
+		} catch (Exception e) {
+			throw new RuntimeException("日期格式不正确:'" + start + "'或'" + end + "'.正确的格式为:'yyyy-MM-dd HH:mm:ss'.");
+		}
+	}
+
+	/**
+	 * 时间差
+	 * 
+	 * @param start
+	 *            开始时间
+	 * @param end
+	 *            结束时间
+	 * @return 时间戳
+	 */
+	public static long dateTimeLag(Object start, Object end) {
+		try {
+			Date s = start instanceof Date ? (Date) start : simpleDateTime.parse((String) start);
+			Date e = end instanceof Date ? (Date) end : simpleDateTime.parse((String) end);
+			return e.getTime() - s.getTime();
+		} catch (Exception e) {
+			throw new RuntimeException("日期格式不正确:'" + start + "'或'" + end + "'.正确的格式为:'yyyy-MM-dd HH:mm:ss'.");
+		}
 	}
 }
